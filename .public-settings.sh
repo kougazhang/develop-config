544919ade29612d6e94018c7ed397a122881f560
kougazhang <kougazhang@gmail.com>
2023-09-08 13:39:19 +0800

    auto commit

---
+++ .public-settings.sh
@@ -0,0 +1,10 @@
+# export homebrew
+export PATH=/opt/homebrew/bin:$PATH
+
+########### About Golang ##############
+# GOPRIVATE is used for pulling private repo
+# export GOPRIVATE=private repo
+# export golang sdk downloaded by goland
+export PATH=/Users/zhangzhao/sdk/go1.21.1/bin:$PATH
+# GOPROXY is the golang package mirror, which can speed up the downloading process 
+export GOPROXY=https://goproxy.cn
